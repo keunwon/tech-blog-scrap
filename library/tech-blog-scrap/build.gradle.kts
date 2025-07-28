add7cccde7a6f787c1cf548e0d7a8e2ff86d3d76
keunwon <keunwon.c2@gmail.com>
2025-07-29 08:20:52 +0900

    Refactor: 셀레니움 삭제

--- library/tech-blog-scrap/build.gradle.kts
+++ library/tech-blog-scrap/build.gradle.kts
@@ -1,7 +1,5 @@
 dependencies {
     implementation(libs.jsoup)
-
     implementation("com.squareup.okhttp3:okhttp:5.1.0")
-    implementation("org.seleniumhq.selenium:selenium-java:4.34.0")
     implementation("org.slf4j:slf4j-api")
 }
